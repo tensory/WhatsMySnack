0f5631fa59af23764f1ba0350082f70f25c5c80e
Ari Lacenski <alacenski@gmail.com>
2017-12-17 22:25:48 -0800

    Dropped Snack.selected property from domain.Snack.

--- app/src/main/java/net/tensory/whatsmysnack/data/models/domain/Snack.kt
+++ app/src/main/java/net/tensory/whatsmysnack/data/models/domain/Snack.kt
@@ -2,4 +2,4 @@ package net.tensory.whatsmysnack.data.models.domain
 
 import net.tensory.whatsmysnack.data.SnackType
 
-data class Snack(val name: String, val type: SnackType, var selected: Boolean = false)
+data class Snack(val name: String, val type: SnackType)
