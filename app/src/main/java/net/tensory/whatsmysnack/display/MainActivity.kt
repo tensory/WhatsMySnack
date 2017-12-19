82680c8792120519762a6ffa097269f8b620bc00
Ari Lacenski <alacenski@gmail.com>
2017-12-19 14:04:07 -0800

    Added menu item to add an item. Added form.

--- app/src/main/java/net/tensory/whatsmysnack/display/MainActivity.kt
+++ app/src/main/java/net/tensory/whatsmysnack/display/MainActivity.kt
@@ -3,14 +3,40 @@ package net.tensory.whatsmysnack.display
 import android.arch.lifecycle.Observer
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import net.tensory.whatsmysnack.R
 import net.tensory.whatsmysnack.SnackApplication
 import net.tensory.whatsmysnack.data.SnackDataProvider
 import net.tensory.whatsmysnack.databinding.ActivityMainBinding
+import net.tensory.whatsmysnack.display.additem.AddItemPresenter
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AddItemPresenter {
+
+    // region AddItemPresenter implementation
+
+    override fun addItem() {
+        AlertDialog.Builder(this)
+                .setView(R.layout.add_item)
+                .show()
+    }
+
+    // endregion
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_add_item -> addItem()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
     @Inject lateinit var snackDataProvider: SnackDataProvider
 
---
+++ app/src/main/java/net/tensory/whatsmysnack/display/additem/AddItemPresenter.kt
@@ -0,0 +1,8 @@
+package net.tensory.whatsmysnack.display.additem
+
+/**
+ * Presenter contract for Add View.
+ */
+interface AddItemPresenter {
+    fun addItem()
+}
