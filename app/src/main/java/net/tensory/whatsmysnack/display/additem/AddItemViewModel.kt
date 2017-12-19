3a5caf73612068179d74c1f7124ea5aeef3f1a93
Ari Lacenski <alacenski@gmail.com>
2017-12-19 15:02:23 -0800

    Set up one-way binding on snackType for Add Item.

--- app/src/main/java/net/tensory/whatsmysnack/display/additem/AddItemViewModel.kt
+++ app/src/main/java/net/tensory/whatsmysnack/display/additem/AddItemViewModel.kt
@@ -1,15 +1,27 @@
 package net.tensory.whatsmysnack.display.additem
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
+import net.tensory.whatsmysnack.BR
 import net.tensory.whatsmysnack.data.SnackType
 
 /**
  * View model for the Add Item UI.
  */
 class AddItemViewModel : BaseObservable() {
+    // This type is automatically bindable.
+    // No further annotation is needed.
     var snackName: String = ""
-    // TODO veggie for now
+
     var snackType: SnackType = SnackType.VEGGIE
+        @Bindable
+        get
+        set(value) {
+            if (field != value) {
+                field = value
+                notifyPropertyChanged(BR.snackType)
+            }
+        }
 
     fun addItem() {
         // TODO insert
